8183df9c13eea0cd902ddcfee9d47d5bab111e99
PaulGCole <50753309+PaulGCole@users.noreply.github.com>
2019-05-21 21:29:41 +0100

    Update homepage.js

--- files/homepage.js
+++ files/homepage.js
@@ -10,6 +10,8 @@ function GetWeather(){
 }
 
 function GotWeather(Http){  
+  if (Http.target.readyState!=4 || Http.target.status!=200 ){return};
+
   var w=JSON.parse(Http.target.responseText);
   document.getElementById("weather").innerHTML=`
      <img class="weathericon" src="https://openweathermap.org/img/w/${w.weather[0].icon}.png" />
@@ -20,7 +22,7 @@ function GotWeather(Http){
      Temp: ${Math.floor(w.main.temp+0.5)} &#176C <span class="weatherminmaxtemp">(${Math.floor(w.main.temp_min+0.5)} - ${Math.floor(w.main.temp_max+0.5)})</span><br> 
      Sunrise: ${timeConverter(w.sys.sunrise)} &nbsp; &nbsp; &nbsp; &nbsp; Sunset: ${timeConverter(w.sys.sunset)}<br>`; 
      
-     
+  document.getElementById("weatherimage").style.backgroundImage = (Date.now() < w.sys.sunset*1000) ? "url('night.bmp')" : "url('day.bmp')"	
 };
 
 function titleCase(str) {
